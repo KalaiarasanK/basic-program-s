9e34b6249fff645f6e01d0fe7d68039f1a4e3805
KalaiarasanK <52280933+KalaiarasanK@users.noreply.github.com>
2019-07-08 15:59:32 +0530

    Create vvowel.py

---
+++ vvowel.py
@@ -0,0 +1,15 @@
+z=input()
+if(z=='A' or z=='a'):
+	print('Vowel')
+elif(z=='E' or z=='e'):
+    print('Vowel')
+elif(z=='I' or z=='i'):
+    print("Vowel")
+elif(z=='O' or z=='o'):
+	print("Vowel")
+elif(z=='U' or z=='u'):
+	print('Vowel')
+elif(z>='a' and z<='z') or (z>='A' and z<='Z'):
+	print('Consonant')
+else:
+	print('invalid')
